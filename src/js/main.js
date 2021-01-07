10d3abce1b64d03716a04303794d78d5d8cf990e
Chris <christina.friede5@gmail.com>
2021-01-08 00:12:49 +0100

    Add same template strings into the main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,26 @@
 "use strict";
 
-const { lstat } = require("fs");
-
 // Eingabedaten holen vom Nutzer
 let titel_1 = prompt('Titel:');
-console.log(titel_1);
+console.log(`Titel: ${titel_1}`);
 let typ_1 = prompt('Typ (Einnahme oder Ausgabe):');
-console.log(typ_1);
-let betrag_1 = prompt('Betrag (in Cent):');
-console.log(betrag_1);
+console.log(`Typ: ${typ_1}`);
+let betrag_1 = parseInt(prompt('Betrag (in Cent):'));
+console.log(`Betrag: ${betrag_1} Cent`);
 let datum_1 = prompt('Datum (jjjj-mm-tt):');
-console.log(datum_1);
+console.log(`Datum: ${datum_1}`);
 
-//Gesamtbilanz
+let titel_2 = prompt('Titel:');
+console.log(`Titel: ${titel_2}`);
+let typ_2 = prompt('Typ (Einnahme oder Ausgabe):');
+console.log(`Typ: ${typ_2}`);
+let betrag_2 = parseInt(prompt('Betrag (in Cent):'));
+console.log(`Betrag: ${betrag_2} Cent`);
+let datum_2 = prompt('Datum (jjjj-mm-tt):');
+console.log(`Datum: ${datum_2}`);
+
+// Gesamtbilanz
 let einnahmen;
 let ausgeben;
-let bilanz;
-
+let bilanz = betrag_1 + betrag_2;
+console.log(`Bilanz: ${bilanz} ct`);
